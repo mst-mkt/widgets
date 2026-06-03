182fe1e9ca1cc25a2ea87e624c77bba8bde757a3
mst-mkt <mail@keito.dev>
2026-06-04 01:41:16 +0900

    fix: adjust position of osd when showing player-panel

--- src/widgets/osd.tsx
+++ src/widgets/osd.tsx
@@ -1,10 +1,15 @@
-import { Astal, type Gdk } from 'ags/gtk4'
+import { Astal, type Gdk, Gtk } from 'ags/gtk4'
 import app from 'ags/gtk4/app'
 
 import { OsdBar } from '../components/osd/osd-bar'
 import { content, visible } from '../stores/osd'
+import { isPanelOpen } from '../stores/panel'
 import { slide } from '../utils/transition'
 import { tweened } from '../utils/tweened'
+
+const MARGIN_DEFAULT = 32
+const PLAYER_HEIGHT = 128
+const MARGIN_ABOVE_PLAYER = MARGIN_DEFAULT + PLAYER_HEIGHT
 
 export const OsdWidget = (gdkmonitor?: Gdk.Monitor) => {
   const { BOTTOM } = Astal.WindowAnchor
@@ -16,6 +21,10 @@ export const OsdWidget = (gdkmonitor?: Gdk.Monitor) => {
   const level = tweened(
     content.as((c) => c.value),
     { duration: 200 },
+  )
+  const playerProgress = tweened(
+    isPanelOpen('player').as((open) => (open ? 1 : 0)),
+    { duration: 280 },
   )
 
   return (
@@ -31,8 +40,16 @@ export const OsdWidget = (gdkmonitor?: Gdk.Monitor) => {
       keymode={Astal.Keymode.NONE}
       application={app}
     >
-      <box class="mx-3 mb-8" css={progress.as(slide([0, 12]))}>
-        <OsdBar icon={content.as((c) => c.icon)} value={level} />
+      <box
+        class="mx-3"
+        halign={Gtk.Align.CENTER}
+        valign={Gtk.Align.END}
+        marginBottom={playerProgress.as((p) => (p < 0.5 ? MARGIN_DEFAULT : MARGIN_ABOVE_PLAYER))}
+        css={progress.as(slide([0, 12]))}
+      >
+        <box css={playerProgress.as((p) => `opacity: ${Math.abs(2 * p - 1)};`)}>
+          <OsdBar icon={content.as((c) => c.icon)} value={level} />
+        </box>
       </box>
     </window>
   )
